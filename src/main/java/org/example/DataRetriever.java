ae1f546928cf064b82a190a2da33b20e68c446d8
Ania-su <antsaaa.randria@gmail.com>
2026-01-03 21:30:35 +0300

    question 6 b)

--- src/main/java/org/example/DataRetriever.java
+++ src/main/java/org/example/DataRetriever.java
@@ -1,11 +1,14 @@
 package org.example;
 
 import org.example.entity.Dish;
+import org.example.entity.Ingredient;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataRetriever {
     private final DBConnection dbconnection;
@@ -36,6 +39,35 @@ public class DataRetriever {
         }
 
         return dish;
+    }
+
+    public List<Ingredient> findIngredients(int page, int size) {
+        List<Ingredient> ingredients = new ArrayList<>();
+
+        int offset = (page - 1) * size;
+
+        String sql = "SELECT d.id, d.name, i.id, i.name FROM Ingredient i limit ? offset ?";
+
+        try (Connection connection = dbconnection.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, offset);
+            statement.setInt(2, size);
+
+            try (ResultSet rs = statement.executeQuery()) {
+                while (rs.next()) {
+                    Ingredient ingredient = new Ingredient();
+                    ingredient.setId(rs.getInt("id"));
+                    ingredient.setName(rs.getString("name"));
+                    ingredients.add(ingredient);
+                }
+
+            }
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return ingredients;
     }
 
 }
